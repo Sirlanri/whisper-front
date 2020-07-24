6157b0a0c2e47cd04fe9c19f7b15b94f094507d3
深蓝PC <mail@ri-co.cn>
2020-07-24 13:23:32 +0800

    添加消息按钮

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -5,6 +5,16 @@
         <v-list-item>
           <v-btn block text large>
             <v-list-item-icon>
+              <v-icon>mdi-bell</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              消息
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block text large>
+            <v-list-item-icon>
               <v-icon>mdi-account-multiple</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
@@ -12,7 +22,6 @@
             </v-list-item-content>
           </v-btn>
         </v-list-item>
-        <v-divider></v-divider>
         <v-list-item>
           <v-btn block text large>
             <v-list-item-icon>
@@ -23,6 +32,7 @@
             </v-list-item-content>
           </v-btn>
         </v-list-item>
+        
       </v-list>
     </v-navigation-drawer>
   </div>
