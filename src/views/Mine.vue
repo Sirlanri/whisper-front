ba5afb0efcdd10f12e6da1acb048e54fbb3940c4
深蓝PC <mail@ri-co.cn>
2020-08-23 18:06:26 +0800

    按钮使用absolute布局

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -6,9 +6,9 @@
       <v-col lg="9">
         <v-card>
           <v-img :src="userData.bannar" class="align-end" height="300">
-            <v-col offset-lg="11">
-              <v-btn class="changeBtn">更改资料</v-btn>
-            </v-col>
+            
+            <v-btn class="changeBtn">更改资料</v-btn>
+            
           </v-img>
           <v-row>
             <v-col lg="2">
@@ -51,7 +51,10 @@ export default {
   border-radius: 100% !important;
   box-shadow:  0px 0px 10px 5px #fff;
 }
-.changeBtn{
-  
+.changeBtn {
+ right: 10px;
+ position: absolute;
+ bottom: 5px;
 }
+
 </style>
