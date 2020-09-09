0684f5cbd55fac758c5df6a1a56de59e48af3937
深蓝 <mail@ri-co.cn>
2020-09-09 19:21:24 +0800

    设定好前端的数据格式

--- src/components/SingleReply.vue
+++ src/components/SingleReply.vue
@@ -4,9 +4,9 @@
     <v-row align-content="center">
       <v-col cols="6">
           <v-avatar size="36">
-            <v-img src="https://pic2.zhimg.com/v2-2dc154cd8b6adcc9af9804b0686e30eb_xl.jpg"></v-img>
+            <v-img :src="avatar"></v-img>
           </v-avatar>
-          回复人昵称
+          {{name}}
       </v-col>
       <v-col cols="2" offset="3">
         <v-btn icon large :color="haveRead?'':'blue'" @click="changRead">
@@ -19,7 +19,7 @@
     <v-row>
       <v-col cols="9">
         <v-card-text>
-          回复的内容
+          {{content}}
         </v-card-text>
       </v-col>
       <v-col cols="3">
@@ -35,8 +35,15 @@
 export default {
   data(){
     return{
-      haveRead:false
+      
     }
+  },
+  props:{
+    name:String,
+    avatar:String,
+    haveRead:Boolean,
+    content:String,
+    id:Number,
   },
   methods:{
     changRead(){
--- src/views/Replys.vue
+++ src/views/Replys.vue
@@ -1,25 +1,65 @@
 <template>
   <div>
     <v-row>
-      
-        <reply></reply>
-        <reply></reply>
-
-        <reply></reply>
+      <reply
+        v-for="(re,index) in replysData"
+        :key="index"
+        :name="re.name"
+        :avatar="re.avatar"
+        :haveRead="re.haveRead"
+        :content="re.content"
+        :id="re.id"
+      ></reply>
     </v-row>
-    
   </div>
 </template>
 
 <script>
-import reply from "@/components/SingleReply.vue"
+import reply from "@/components/SingleReply.vue";
 export default {
-  components:{
-    reply
-  }
-}
+  components: {
+    reply,
+  },
+  data() {
+    return {
+      replysData: [
+        {
+          id: 1,
+          name: "深蓝蓝",
+          avatar:
+            "https://pic2.zhimg.com/v2-2dc154cd8b6adcc9af9804b0686e30eb_xl.jpg",
+          haveRead: false,
+          content: "回复的内容在这儿~",
+        },
+        {
+          id: 2,
+          name: "深蓝蓝",
+          avatar:
+            "https://pic2.zhimg.com/v2-2dc154cd8b6adcc9af9804b0686e30eb_xl.jpg",
+          haveRead: false,
+          content: "回复的内容在这儿~",
+        },
+        {
+          id: 3,
+          name: "深蓝蓝",
+          avatar:
+            "https://pic2.zhimg.com/v2-2dc154cd8b6adcc9af9804b0686e30eb_xl.jpg",
+          haveRead: false,
+          content: "回复的内容在这儿~",
+        },
+        {
+          id: 4,
+          name: "深蓝蓝",
+          avatar:
+            "https://pic2.zhimg.com/v2-2dc154cd8b6adcc9af9804b0686e30eb_xl.jpg",
+          haveRead: false,
+          content: "回复的内容在这儿~",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
